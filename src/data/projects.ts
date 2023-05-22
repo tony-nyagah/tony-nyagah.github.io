be15b2e947e710483618a7eda33470dc8510718a
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-05-22 17:25:32 +0300

    Updated the index page

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: "Newsletter Signup Page",
+    img: "/newsletter_signup.png",
+    desc: "A simple newsletter signup page made using Express, Bootstrap, the Mailchimp API and EJS.",
+    url: "https://ostrich-newsletter-signup.onrender.com/",
+    badge: "Frontend",
+  },
+  {
+    title: "Phonecat",
+    img: "/phonecat.png",
+    desc: "A phone catalog application made using Vue 3.",
+    url: "https://vue-phonecat.vercel.app/",
+    badge: "Frontend",
+  },
+];
