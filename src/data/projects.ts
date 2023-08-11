4aaa690fc4b82638fc305adecc6ccaebaca74079
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-08-11 16:00:25 +0300

    added my alx week 14 stuff

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,19 @@
 export const projects = [
   {
+    title: "Team Project Video",
+    img: "/team_project_presentation_video.png",
+    desc: "A video presentation made by my ALX team, Group 35, on how to curb unemployment among the youth through growing exotic fruits.",
+    url: "https://www.youtube.com/watch?v=JoxZy-aHGdk",
+    badge: "Other",
+  },
+  {
+    title: "Team Project Presentation Slides",
+    img: "/team_project_presentation_slides.png",
+    desc: "A presentation made by my ALX team, Group 35, on how to curb unemployment among the youth through growing exotic fruits.",
+    url: "https://drive.google.com/file/d/1tYg7O75X7F7aqCmpyZBN_38wapoFc5vS/",
+    badge: "Other",
+  },
+  {
     title: "Newsletter Signup Page",
     img: "/newsletter_signup.png",
     desc: "A simple newsletter signup page made using Express, Bootstrap, the Mailchimp API and EJS.",
