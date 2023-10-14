7028355da9261232412b56abc2d2054cfaa21544
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-10-14 15:40:42 +0300

    revert the main branch to hoew it should be

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,25 +1,11 @@
 export const projects = [
   {
-    title: "Team Project Video",
-    img: "/team_project_presentation_video.png",
-    desc: "A video presentation made by my ALX team, Group 35, on how to curb unemployment among the youth through growing exotic fruits.",
-    url: "https://www.youtube.com/watch?v=JoxZy-aHGdk",
-    badge: "Other",
+    title: "Newsletter Signup Page",
+    img: "/newsletter_signup.png",
+    desc: "A simple newsletter signup page made using Express, Bootstrap, the Mailchimp API and EJS.",
+    url: "https://ostrich-newsletter-signup.onrender.com/",
+    badge: "Frontend",
   },
-  {
-    title: "Team Project Presentation Slides",
-    img: "/team_project_presentation_slides.png",
-    desc: "A presentation made by my ALX team, Group 35, on how to curb unemployment among the youth through growing exotic fruits.",
-    url: "https://drive.google.com/file/d/1tYg7O75X7F7aqCmpyZBN_38wapoFc5vS/",
-    badge: "Other",
-  },
-  // {
-  //   title: "Newsletter Signup Page",
-  //   img: "/newsletter_signup.png",
-  //   desc: "A simple newsletter signup page made using Express, Bootstrap, the Mailchimp API and EJS.",
-  //   url: "https://ostrich-newsletter-signup.onrender.com/",
-  //   badge: "Frontend",
-  // },
   {
     title: "Phonecat",
     img: "/phonecat.png",
