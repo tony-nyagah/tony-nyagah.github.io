0e1501a0684c5e26699e9ed91789d40429fc9953
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-02-29 11:11:32 +0300

    remake the layout - use astro's default blog template

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Nyagah * Dev";
+export const SITE_DESCRIPTION = "Welcome to Antony Nyagah's personal website!";
