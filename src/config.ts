11838f0676ea4d7222d3dfdba6ab47d43780b557
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-02-29 09:41:41 +0300

    a fresh start for my own work

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Antony Nyagah";
-export const SITE_DESCRIPTION =
-  "This is Antony Nyagah's little space on the internet.";
