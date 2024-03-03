ae3c6f843df58063419de0692317e4051ec50089
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-03-03 09:23:46 +0300

    another fresh start

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Nyagah * Dev";
-export const SITE_DESCRIPTION = "Welcome to Antony Nyagah's personal website!";
---
+++ src/data/blog-posts/why-i'm-learning-go.md
@@ -0,0 +1,5 @@
+---
+title: Why I am learning Go
+publishDate: 02 Mar 2024
+description: My journey as a software developer has lead me to Go
+---
